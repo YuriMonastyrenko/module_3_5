7362df483d167420f789b8a127e8297cc6caca69
Yura <monastyrenko99@mail.ru>
2024-08-31 11:54:49 +0300

    Update Module_3_5.py

--- Module_3_5.py
+++ Module_3_5.py
@@ -1,5 +1,5 @@
 def get_multiplied_digits(number):
-    str_number = str(number)
+    str_number = str(int(number))
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
@@ -8,7 +8,7 @@ def get_multiplied_digits(number):
 
 result1 = get_multiplied_digits(40203)
 result2 = get_multiplied_digits(100000000067088)
-result3 = get_multiplied_digits(1)
+result3 = get_multiplied_digits("0092")
 print(result1)
 print(result2)
 print(result3)
